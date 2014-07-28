d7cbb03296678c7f91b1f2429cbad36f1bc939f2
Jack McCracken <jackamc19@gmail.com>
2014-07-27 18:17:41 -0600

    Added BMP format

--- app/controllers/print_tools_controller.rb
+++ app/controllers/print_tools_controller.rb
@@ -7,7 +7,7 @@ class PrintToolsController < ApplicationController
   # POST
   def do_print
     ext_convert = [".odt", ".docx", ".doc"]
-    ext_fine = [".png", ".pdf", ".jpeg"]
+    ext_fine = [".png", ".pdf", ".jpeg", ".bmp"]
     supported_exts = ext_convert + ext_fine
     
     uploaded_io = params[:to_print]
